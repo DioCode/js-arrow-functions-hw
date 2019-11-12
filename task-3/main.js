48e8e8db181614495cdf719a1bb9bd05ac375002
Denys Semenov <54813799+DioCode@users.noreply.github.com>
2019-11-12 17:13:53 +0200

    added another solution of task-3

--- task-3/main.js
+++ task-3/main.js
@@ -1,3 +1,12 @@
-const reverse = arr => arr.reduceRight((first, second) => (first.push(second), first), []);
+// const reverse = arr => arr.reduceRight((first, second) => (first.push(second), first), []);
+const reverse = arr => {
+  let reversedArr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversedArr.push(arr[i])
+  }
+  return [reversedArr, arr]
+}
 
 console.log(reverse(['a', 'b', 'c', 'd', 'e']));
+
+// make it with cycle for!
